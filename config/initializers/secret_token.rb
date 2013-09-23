ff9eee861b91c1a967d3e6ef6825c3884164ccfe
Blake Rainwater <brainwater3@gatech.edu>
2013-09-23 18:17:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pbctl::Application.config.secret_key_base = '92a0b4b7f4be77fd3ead85989939d44dee591e3e42df2722f933d1147c6143d62cd2a85812031f69da0350bdfce8b492c872638a664cdce8ad132bfe27baf8df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pbctl::Application.config.session_store :cookie_store, key: '_pbctl_session'
