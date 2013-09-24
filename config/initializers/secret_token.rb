af522c5486a61dc1869baaff421e59309442d048
Blake Rainwater <brainwater3@gatech.edu>
2013-09-24 13:42:31 -0400

    Changed secret_token to be created on application start

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Pbctl::Application.config.secret_key_base = '92a0b4b7f4be77fd3ead85989939d44dee591e3e42df2722f933d1147c6143d62cd2a85812031f69da0350bdfce8b492c872638a664cdce8ad132bfe27baf8df'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
