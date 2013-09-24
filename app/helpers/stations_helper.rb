3b7b05ab627ee35f756eb3a95d5d8f77d15f5a11
Blake Rainwater <brainwater3@gatech.edu>
2013-09-24 15:33:45 -0400

    Prevent repeated songs from being input into database

--- app/helpers/stations_helper.rb
+++ app/helpers/stations_helper.rb
@@ -6,29 +6,33 @@ module StationsHelper
 
   def process_fill(text)
     stationarray = []
-    Song.create
+    lastsong = Song.last
+    newsong = Song.new
     text.each_line do |block|
       if block.match('station[0-9]+=.*')
         stationarray << block[/station[0-9]+=(.*)/,1]
       elsif block.match('coverArt=.*')
         # Cover Art
-        current_song.update_attributes(art: block[/coverArt=(.*)/,1])
+        newsong.art = block[/coverArt=(.*)/,1]
       elsif block.match('detailUrl=.*')
         # Detail Url
-        current_song.update_attributes(detail: block[/detailUrl=(.*)/,1])
+        newsong.detail = block[/detailUrl=(.*)/,1]
       elsif block.match('title=.*')
         # Title
-        current_song.update_attributes(title: block[/title=(.*)/,1])
+        newsong.title = block[/title=(.*)/,1]
       elsif block.match('artist=.*')
         # Artist
-        current_song.update_attributes(artist: block[/artist=(.*)/,1])
+        newsong.artist = block[/artist=(.*)/,1]
       elsif block.match('album=.*')
         # Album
-        current_song.update_attributes(album: block[/album=(.*)/,1])
+        newsong.album = block[/album=(.*)/,1]
       elsif block.match('rating=[0-9]+')
         # Rating
-        current_song.update_attributes(rating: block[/rating=([0-9]+)/,1])
+        newsong.rating = block[/rating=([0-9]+)/,1]
       end
+    end
+    if !newsong.title.empty? && ( newsong.title != lastsong.title || newsong.artist != lastsong.artist || newsong.album != lastsong.album )
+      newsong.save
     end
     #if stationarray.length > 0
     #  Station.delete_all
